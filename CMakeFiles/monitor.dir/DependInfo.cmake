0c002c5b21a878df607083f5d199b520c9cc4fe2
AodOmar <ayoub.oudada@uit.ac.ma>
2023-07-18 17:19:36 +0100

    add main function

---
+++ CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/omar/J2EE/projects/System-activity-monitor/src/format.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/linux_parser.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/main.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/ncurses_display.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/process.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/processor.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/omar/J2EE/projects/System-activity-monitor/src/system.cpp" "/home/omar/J2EE/projects/System-activity-monitor/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
